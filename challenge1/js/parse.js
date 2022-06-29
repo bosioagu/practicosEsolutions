d365f2b88fb5e0ab0f17cfcd2083d57e0473d089
Agustín Bosio <bosio.agustinariel@gmail.com>
2022-06-29 17:20:38 -0300

    Corrección tipos de datos

--- challenge1/js/parse.js
+++ challenge1/js/parse.js
@@ -5,9 +5,10 @@
  * El codigo convierte un JSON determinado a un objeto JavaScript
  */
 
- const json   = {};      // En esta variable se guarda el objeto final
+const json   = {};      // En esta variable se guarda el objeto final
  const button = document.querySelector("button");
  
+
  const convert = () => {
    let textarea        = document.querySelector("textarea").value;
    let removeSpaces    = textarea.trim().slice(1).slice(1, -1).replace(/\n+/g, "");
@@ -51,13 +52,14 @@
                subObject = {}
                k++
              }
- 
+             
          json[subString.replace(/['"]+/g, "").replace(/,/g, "").trim()] =  newArray
          leftover= leftover.substring(leftover.indexOf("]")+1,leftover.length)
- 
+
          } else {
            leftover = leftover.substring(separator2, leftover.length);
            json[subString.replace(/['"]+/g, "").replace(/,/g, "").trim()] =  subString2.slice(1).replace(/['"]+/g, "").trim();
+
          }
  
        } else {
@@ -72,7 +74,43 @@
    } while (breakingPoint == true);
  
  console.log("El objeto 'json' ha sido creado")
+
+ correctTypeof()
  }; 
  
+
+// La funcion correctTypeof() corrige los tipos de datos 
+function correctTypeof() {
+  let jsonObject = json
+  let testNumber = /^\d*(\.\d{1})?\d{0,1}$/;
+
+  for ( element in jsonObject) {
+
+    if (jsonObject[element] === "null")jsonObject[element] = null;
+    if (jsonObject[element]=== "true")jsonObject[element] = true; 
+    if (jsonObject[element] === "false")jsonObject[element] = false; 
+    if (testNumber.test(jsonObject[element])) jsonObject[element] = parseFloat(jsonObject[element]); 
+
+    let internalArray = jsonObject[element]
+
+    if ( typeof internalArray === 'object' &&  internalArray !== null) { 
+      for(internalElement in internalArray){
+
+        let internalObject = internalArray[internalElement]
+
+        for (internalElement in internalObject) {
+          if (internalObject[internalElement] === "null")internalObject[internalElement] = null;
+          if (internalObject[internalElement]=== "true")internalObject[internalElement] = true; 
+          if (internalObject[internalElement] === "false")internalObject[internalElement] = false; 
+          if (testNumber.test(internalObject[internalElement])) internalObject[internalElement] = parseFloat(internalObject[internalElement]); 
+
+        }
+      }
+    }
+
+  } 
+}
+correctTypeof()
+  
+
  button.addEventListener("click", convert);
- 
